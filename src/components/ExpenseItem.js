d3166db0c5b57ab068e44dec54a123d89e061acf
dattatreyav30 <dattatreya361@gmail.com>
2023-05-24 21:38:33 +0530

    understanding props in depth and using multiple file system to make code readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <ExpenseDetails
+          title={props.title}
+          location={props.location}
+          amount={props.amount}
+        />
       </div>
     </div>
   );
